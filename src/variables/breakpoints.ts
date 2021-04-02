12a57f685601a906b1e549486a5e674e2d3c0437
Nick Zou <z.nick00@gmail.com>
2021-04-02 01:12:22 -0400

    adds breakpoints

---
+++ src/variables/breakpoints.ts
@@ -0,0 +1,9 @@
+const breakpoints:any = {
+    phablet: `(min-width: 576px)`,
+    tablet: `(min-width: 768px)`,
+    laptop: `(min-width: 992px)`,
+    desktop: `(min-width: 1200px)`,
+    desktoparge: `(min-width: 1400px)`
+};
+
+export default breakpoints;

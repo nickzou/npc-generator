6a6e976fc8af9b211e208f6c6327c3cdd375c2ce
Nick Zou <z.nick00@gmail.com>
2021-04-02 19:05:48 -0400

    add tablet and laptop styles

--- src/components/NPCListItem/NPCListItem.tsx
+++ src/components/NPCListItem/NPCListItem.tsx
@@ -2,9 +2,18 @@
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
+//Internal Utilities
+import breakpoints from '../../variables/breakpoints';
+
 const Wrapper = styled.div`
     position: relative;
     background-color: red;
+    @media ${breakpoints.laptop} {
+        width: 400px;
+        height: 550px;
+        display: flex;
+        flex-direction: column;
+    }
 `;
 
 const ImageWrapper = styled.div`
@@ -12,24 +21,61 @@ const ImageWrapper = styled.div`
     width: 100%;
     height: 0;
     padding-top: 35.5%;
+    @media ${breakpoints.tablet} {    
+        padding-top: 29.3%;
+    }
+    @media ${breakpoints.laptop} {
+        padding-top: 0;
+        height: 340px;
+        flex-shrink: 0;
+    }
 `;
 
 const ContentWrapper = styled.div`
-    width: 100%;
+    position: relative;
+    width: calc(100% - 24px);
     background-color: white;
-    display: grid;
-    grid-template-columns: 1fr;
-    grid-template-rows: 1fr 1fr;
-    row-gap: 6px;
+    display: flex;
+    flex-direction: column;
     padding: 12px;
+    @media ${breakpoints.laptop} {
+        flex-grow: 1;
+        text-align: center;
+        justify-content: center;
+        padding-bottom: 60px;
+    }
 `;
 
 const Name = styled.h2`
     font-size: 21px;
+    margin-bottom: 6px;
+    @media ${breakpoints.tablet} {    
+        font-size: 26px;
+    }
 `;
 
 const Subtitle = styled.span`
     font-size: 14px;
+`;
+
+const ButtonWrapper = styled.div`
+    position: absolute;
+    top: 0;
+    right: 12px;
+    transform: translateY(-50%);
+    display: flex;
+    @media ${breakpoints.laptop} {
+        top: initial;
+        bottom: 30px;
+        right: 50%;
+        transform: translate(50%, -50%);
+    }
+    button {
+        margin-right: 8px;
+        &:last-of-type {
+            margin-right: 0;
+        }
+    }
 `;
 
 const NPCListItem = (props:any) => {
@@ -39,6 +85,10 @@ const NPCListItem = (props:any) => {
             <ContentWrapper>
                 <Name className="almendra">{props.name}</Name>
                 <Subtitle className="pt-serif">{props.age}/{props.species}</Subtitle>
+                <ButtonWrapper>
+                    <button>Save</button>
+                    <button>Stats</button>
+                </ButtonWrapper>
             </ContentWrapper>
         </Wrapper>
     )
